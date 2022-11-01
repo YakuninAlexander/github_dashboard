e16cc9c7542992a853f194962622916a604653ab
YakuninAlexander <wepqwppw@gmail.com>
2022-11-01 17:37:04 +0300

    Добавлена основная работа с api.github

---
+++ src/redux/github/github.api.ts
@@ -0,0 +1,31 @@
+import { IRepo } from '../../interfaces/repositories'
+import { ISearchRepos } from '../../interfaces/repositories';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+
+// Define a service using a base URL and expected endpoints
+export const githubApi = createApi({
+  reducerPath: 'github/api',
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://api.github.com' }),
+  endpoints: (builder) => ({
+    getRepos: builder.query<ISearchRepos, string>({
+      query: (name) => ({
+        url:`search/repositories`,
+        params: {
+          q: name,
+          per_page: 10,
+          sort: 'stars',
+        }
+      }),
+    }),
+    getUserRepo: builder.query<IRepo, string>({
+      query: (url) => ({
+        url,
+      })
+    })
+  }),
+})
+
+// Export hooks for usage in functional components, which are
+// auto-generated based on the defined endpoints
+export const { useGetReposQuery, useGetUserRepoQuery } = githubApi
